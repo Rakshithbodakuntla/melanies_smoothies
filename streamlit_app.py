0bc3d146b214b1f6bfdad717bb4c5aacee4831d8
Bodakuntla Rakshith <bodakuntlarakshith1@gmail.com>
2026-03-16 13:22:31 -0500

    Refactor Snowflake session connection in app
    Updated connection method to use Streamlit's connection feature for Snowflake.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+
 
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie:")
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 fruit_rows = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME")).collect()
 fruit_list = [row["FRUIT_NAME"] for row in fruit_rows]
