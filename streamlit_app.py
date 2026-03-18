bf2cac69566739ff6a96140618fb790736fc1907
Bodakuntla Rakshith <bodakuntlarakshith1@gmail.com>
2026-03-18 01:00:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,6 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information') 
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen) 
         sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width= True) 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width= True)
+#sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width= True)
